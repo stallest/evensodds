5d67edb13b18a9deb0296fdb3ab4e309a723e0b2
Spencer Tall <tall.spencer@gmail.com>
2019-07-02 15:27:32 -0600

    commit

--- tests/evensodds.js
+++ tests/evensodds.js
@@ -1,10 +1,12 @@
 module.exports = {
     beforeEach: browser => {
-        browser.url: ('https://livepickleball.com/#/')
+        browser.url: ('https://devmountain-qa.github.io/Automation-Basics/build/')
     },
     after: browser => {
 
     },
     'Login': browser => {
         browser
+            .click('input[name="evenOddInput"]')
+            .setValue('input[name="evenOddInput"]', '')
     }
